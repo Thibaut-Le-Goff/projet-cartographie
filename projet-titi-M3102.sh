d8d285d302bb2761f7c45b31feae18f5f81583b0
Thibaut-Le-Goff <90935476+Thibaut-Le-Goff@users.noreply.github.com>
2022-01-16 16:23:13 +0100

    Update projet-titi-M3102.sh

--- projet-titi-M3102.sh
+++ projet-titi-M3102.sh
@@ -4,7 +4,6 @@ x=0
 y=0
 
 rm carte-titi-projet-M3102.dot
-rm carte-titi-projet-M3102.png
 rm carte-titi-projet-M3102.txt
 rm dot-titi-projet-M3102
 rm dot2-titi-projet-M3102
@@ -12,35 +11,35 @@ rm dot2-titi-projet-M3102
 echo "digraph {" > dot-titi-projet-M3102.txt
 echo "  Protocols -> UDP" >> dot-titi-projet-M3102.txt
 echo "  Protocols -> TCP" >> dot-titi-projet-M3102.txt
-echo "  UDP -> Time_Protocol [color=red]" >> dot-titi-projet-M3102.txt #37
+echo "  UDP -> Network_Time_Protocol [color=red]" >> dot-titi-projet-M3102.txt #123
 echo "  UDP -> DNS [color=orange]" >> dot-titi-projet-M3102.txt #53
 echo "  UDP -> HTTP [color=yellow]" >> dot-titi-projet-M3102.txt #80
 echo "  UDP -> RIP [color=green]" >> dot-titi-projet-M3102.txt #520
 echo "  UDP -> RADIUS_accounting_protocol [color=blue]" >> dot-titi-projet-M3102.txt #1813
 echo "  UDP -> MQTT [color=purple]" >> dot-titi-projet-M3102.txt #1883
 echo "  UDP -> AWS [color=pink]" >> dot-titi-projet-M3102.txt #4195
+echo "  UDP -> DHCP_Client [color=violet]" >> dot-titi-projet-M3102.txt #68
 echo "  TCP -> FTP_data_transfer [color=brown]" >> dot-titi-projet-M3102.txt #20
 echo "  TCP -> Telnet_over_TLS_SSL [color=chartreuse]" >> dot-titi-projet-M3102.txt #992
 echo "  TCP -> SMTP [color=crimson]" >> dot-titi-projet-M3102.txt #25
-echo "  TCP -> Time_Protocol [color=red3]" >> dot-titi-projet-M3102.txt #37
+echo "  TCP -> LDAP [color=lawngreen]" >> dot-titi-projet-M3102.txt #389
 echo "  TCP -> DNS [color=orange3]" >> dot-titi-projet-M3102.txt #53
 echo "  TCP -> HTTP [color=yellow3]" >> dot-titi-projet-M3102.txt #80
 echo "  TCP -> SMTP_chiffrement_explicite [color=grey]" >> dot-titi-projet-M3102.txt #587
-echo "  TCP -> DHCP_Failover [color=violet]" >> dot-titi-projet-M3102.txt #647
 echo "  TCP -> RADIUS_accounting_protocol [color=blue4]" >> dot-titi-projet-M3102.txt #1813
 echo "  TCP -> MQTT [color=purple3]" >> dot-titi-projet-M3102.txt #1883
 echo "  TCP -> GNUnet [color=fuchsia]" >> dot-titi-projet-M3102.txt #2086
 echo "  TCP -> Docker_REST_API_plain [color=gold]" >> dot-titi-projet-M3102.txt #2375
 echo "  TCP -> AWS [color=pink3]" >> dot-titi-projet-M3102.txt #4195
 
-push-dot () {	
+push-dot () {
 	traceroute -n $1 -p $2 -A $i | tr -d '*' | cut -c 3- | tail -n +2 | cut -d " " -f 3-5 | tr -d ' ' | sed '/^[[:space:]]*$/d' > carte-titi-projet-M3102.txt
 	src="Mon PC"
 	carte=$(cat carte-titi-projet-M3102.txt)
 	rm carte-titi-projet-M3102.txt
 	for z in $carte
 	do
-	        echo "  "\"$src"\" -> "\"$z"\" [color="\"$3"\"]" >> dot-titi-projet-M3102.txt 
+	        echo "  "\"$src"\" -> "\"$z"\" [label="\"$i"\"] [color="\"$3"\"]" >> dot-titi-projet-M3102.txt 
 	        src=$z
 	done
 	echo "}" >> dot-titi-projet-M3102.txt
@@ -61,8 +60,8 @@ push-dot () {
 final-push () {
 	push-dot -T 20 brown
 	push-dot -T 25 crimson
-	push-dot -T 37 red3
-	push-dot -U 37 red
+	push-dot -T 389 lawngreen
+	push-dot -U 123 red
 	push-dot -T 992 chartreuse
 	push-dot -T 53 orange3
 	push-dot -U 53 orange
@@ -70,7 +69,7 @@ final-push () {
 	push-dot -U 80 yellow
 	push-dot -U 520 green
 	push-dot -T 587 grey
-	push-dot -T 647 violet
+	push-dot -U 68 violet
 	push-dot -T 1813 blue4
 	push-dot -U 1813 blue
 	push-dot -T 1883 purple3
